6bef10724b6bc78501c8cb515da742dbfe221d6e
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-05 08:49:24 -0700

    Create display method for Board class

--- lib/board.rb
+++ lib/board.rb
@@ -5,4 +5,13 @@ class Board
   def initialize
     @grid = Array.new(3, Array.new(3, '-'))
   end
+
+  def display
+    puts "
+     #{@grid[0][0]} | #{@grid[0][1]} | #{@grid[0][2]}
+    -----------
+     #{@grid[1][0]} | #{@grid[1][1]} | #{@grid[1][2]}
+    -----------
+     #{@grid[2][0]} | #{@grid[2][1]} | #{@grid[2][2]}"
+  end
 end
