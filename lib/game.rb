e17f6b61534ea528cfe6941bc8760119f6ed316e
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-05 08:48:56 -0700

    Populate Game class with some instance variables and methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 
+require_relative 'board'
+
 # Game class contains game logic of tic-tac-toe
 class Game
-  def initialize(player1, player2)
-    # some code
+  attr_accessor :player1, :player2, :board, :win_cons
+
+  @win_cons = [
+    ['0,0', '0,1', '0,2'], ['1,0', '1,1', '1,2'], ['2,0', '2,1', '2,2'],
+    ['0,0', '1,0', '2,0'], ['0,1', '1,1', '2,1'], ['0,2', '1,2', '2,2'],
+    ['0,0', '1,1', '2,2'], ['0,2', '1,1', '2,0']
+  ]
+
+  def initialize(player1, player2, board)
+    @player1 = player1
+    @player2 = player2
+    @board = board
   end
+
+  def display_board
+    board.display
+  end
+  
 end
