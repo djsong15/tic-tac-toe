e09fa4aea01263626d022922ba5f85094057a9a4
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-06 22:04:11 -0700

    Start writing game logic; player 1 selects a symbol, player 2 defaults to other

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
 require_relative 'board'
+require_relative 'player'
 
 # Game class contains game logic of tic-tac-toe
 class Game
-  attr_accessor :player1, :player2, :board, :win_cons
-
-  @win_cons = [
-    ['0,0', '0,1', '0,2'], ['1,0', '1,1', '1,2'], ['2,0', '2,1', '2,2'],
-    ['0,0', '1,0', '2,0'], ['0,1', '1,1', '2,1'], ['0,2', '1,2', '2,2'],
-    ['0,0', '1,1', '2,2'], ['0,2', '1,1', '2,0']
-  ]
+  attr_accessor :players, :board, :win_cons, :turn
 
   def initialize(player1, player2, board)
-    @player1 = player1
-    @player2 = player2
+    @players = [player1, player2]
     @board = board
+    @win_cons = [
+      ['0,0', '0,1', '0,2'], ['1,0', '1,1', '1,2'], ['2,0', '2,1', '2,2'],
+      ['0,0', '1,0', '2,0'], ['0,1', '1,1', '2,1'], ['0,2', '1,2', '2,2'],
+      ['0,0', '1,1', '2,2'], ['0,2', '1,1', '2,0']
+    ]
+    @turn = player1.symbol
   end
 
-  def display_board
+  def play_game
+    puts "Player 1 is #{@players[0].symbol}, Player 2 is #{@players[1].symbol}"
     board.display
+    puts "#{@turn}, pick your spot \n"
+    spot = gets.chomp
+    board.update(spot, @turn)
   end
   
 end
