8e9b64d1a807b7ec8ad07f0bd2fc793c1a51abb4
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-07 11:17:29 -0700

    Modify Board methods - display adds an extra line break, update correctly updates grid with sym position

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,37 @@
 
 # Boar class contains initializes tic tac toe board
 class Board
+  attr_accessor :grid
+
   def initialize
-    @grid = Array.new(3, Array.new(3, '-'))
+    @grid = [
+      %w[1 2 3],
+      %w[4 5 6],
+      %w[7 8 9]
+    ]
   end
 
   def display
-    puts "
-     #{@grid[0][0]} | #{@grid[0][1]} | #{@grid[0][2]}
-    -----------
-     #{@grid[1][0]} | #{@grid[1][1]} | #{@grid[1][2]}
-    -----------
-     #{@grid[2][0]} | #{@grid[2][1]} | #{@grid[2][2]}"
+    puts "\n"
+    @grid.each_with_index do |row, r_i|
+      row_str = ' '
+      row.each_with_index do |col, c_i|
+        row_str += "#{col} "
+        row_str += '| ' if c_i < 2
+      end
+      puts row_str
+      puts '-----------' if r_i < 2
+    end
+  end
+
+  def update(spot, symbol)
+    r = (spot.to_i - 1) / 3
+    c = (spot.to_i - 1) % 3
+    if @grid[r][c] == 'X' || @grid[r][c] == 'O'
+      puts 'Taken, pick another spot'
+      update(gets.chomp, symbol)
+    else
+      @grid[r][c] = symbol
+    end
   end
 end
